27feb015ce0370974757210b68032afd9e2e7f5a
Gaston Walker <gastonw118@gmail.com>
2023-07-27 06:07:28 +0500

    Completed functionality to clone git repos

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, child_process: false };
+
+    return config;
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
 import Head from 'next/head';
+import {useState} from 'react';
+import simpleGit from 'simple-git';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+
+  const [url, setUrl] = useState("");
+
+  const handleURLInput = (e) => {
+    setUrl(e.target.value);
+  }
+
+  const handleCloningAction = async () => {
+    try {
+      await simpleGit().clone(url, './clonned-code');
+      // Make your changes here, e.g., modify a file
+      // fs.writeFileSync('./clonned-code/app/page.js', 'Modified content');
+      await simpleGit('./clonned-code').add('./*');
+      await simpleGit('./clonned-code').commit('Updating ');
+      await simpleGit('./clonned-code').push();
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,53 +33,35 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Git Simulator in NextJs!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          <code>Let's clone your repo:</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+          <input 
+            className={styles.urlField}
+            type="text"
+            name="git-url"
+            value={url}
+            placeholder="Paste your git repo link here"
+            onChange={handleURLInput}
+          />
+          <button 
+            className={styles.cloneBtn}
+            onClick={handleCloningAction}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Clone
+          </button>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <h3>
+          Adaptavist
+        </h3>
       </footer>
 
       <style jsx>{`
